519f05b23f4e8b6fb3886e2cc9e249a23f073e32
Achmadsetiawan <achmadsetiawannn@gmail.com>
2019-08-25 00:25:14 +0700

    Second Commit

--- app/src/main/java/com/example/myintentapp/MainActivity.java
+++ app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -8,12 +8,17 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /* Deklarasi Layout activity_main name */
+
     Button btnMoveActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Inisiasi R.id Layout activity_main menjadi String ada MainActivity */
 
         btnMoveActivity = findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
@@ -22,6 +27,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public  void onClick(View v) {
         switch (v.getId()) {
+
+            /*
+            Deklarasi R.id.move_activity sebagai key untuk pindah halaman class
+            dari MainActivity.class ke MoveActivity.class
+            */
+
             case R.id.btn_move_activity:
                 Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
                 startActivity(moveIntent);
